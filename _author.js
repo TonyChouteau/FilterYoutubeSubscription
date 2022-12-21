d5a463fdd49359ee1125e85bcc71f8ff288b1873
root <root@DESKTOP-VCAQ2T8>
2022-12-21 23:11:36 +0100

    initial

---
+++ _author.js
@@ -0,0 +1,7 @@
+// =======================================
+//   /!\  Rename the file author.js  /!\ 
+// =======================================
+
+const author_to_hide = [
+    "PewDiePie", // Example
+];
